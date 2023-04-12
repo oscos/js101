1018b11d7656bb7204223f4256238b580171831a
oscos <cortes.oscar@outlook.com>
2023-04-11 21:55:20 -0700

    initial commit

---
+++ exercises/medium_1/x04-stack-machine-interpretation.js
@@ -0,0 +1,108 @@
+/*
+  Created: 04/06/2023
+  Updated:
+  Launch School - Intro to Javascript
+  JS101-JS119 - Small Problems > Medium 1 > 
+
+  Instructions:
+
+  **********************************START PEDAC********************************
+  -----------------------------------PROBLEM-----------------------------------
+  Input: 
+
+  Output: 
+  ----------------------------------RULES--------------------------------------
+  Explicit:
+
+  Implicit: 
+
+  EdgeCases: 
+    i.e.
+    - empty or null or None values
+    - zero or negative values
+
+  ---------------------------------MENTAL MODEL--------------------------------
+  -----------------------------------EXAMPLES----------------------------------
+  --------------------------------DATA STRUCTURES------------------------------
+  -----------------------------------ALGORITHM---------------------------------
+  -------------------------------CODE WITH INTENT------------------------------
+  **********************************END PEDAC**********************************
+*/
+
+const minilang = (str) => {
+  const arr = str.split(" ");
+  // console.dir(arr);
+
+  let stack = [];
+  let register = 0;
+
+  arr.forEach((n) => {
+    // console.dir(`n => ${n} : stack => ${stack} : register => ${register}`)
+    switch (n) {
+      case "ADD":
+        register += stack.pop();
+        break;
+      case "SUB":
+        register -= stack.pop();
+        break;
+      case "MULT":
+        register *= stack.pop();
+        break;
+      case "DIV":
+        register = Math.round(register / stack.pop());
+        break;
+      case "REMAINDER":
+        register = Math.round(register % stack.pop());
+        break;
+      case "PUSH":
+        stack.push(register);
+        break;
+      case "POP":
+        register = stack.pop();
+        break;
+      case "PRINT":
+        console.log(register);
+        break;
+      default:
+        register = parseInt(n);
+    }
+  });
+};
+
+minilang("3 PUSH PUSH 7 DIV MULT PRINT");
+// 6
+
+/*
+minilang("PRINT");
+// 0
+
+minilang("5 PUSH 3 MULT PRINT");
+// 15
+
+minilang("5 PRINT PUSH 3 PRINT ADD PRINT");
+// 5
+// 3
+// 8
+
+minilang("5 PUSH POP PRINT");
+// 5
+
+minilang("3 PUSH 4 PUSH 5 PUSH PRINT ADD PRINT POP PRINT ADD PRINT");
+// 5
+// 10
+// 4
+// 7
+
+minilang("3 PUSH PUSH 7 DIV MULT PRINT");
+// 6
+
+minilang("4 PUSH PUSH 7 REMAINDER MULT PRINT");
+// 12
+
+minilang("-3 PUSH 5 SUB PRINT");
+// 8
+
+console.log(minilang("6 PUSH"));
+// (nothing is printed because the `program` argument has no `PRINT` commands)
+
+*/
